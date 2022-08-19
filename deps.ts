83bfbd73427b6aa6eeb73d96bfe9a3f5606ede35
yskszk63 <yskszk63@gmail.com>
2022-08-19 22:05:19 +0900

    Update

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import * as path from "https://deno.land/std@0.152.0/path/mod.ts";
-import { yamlParse } from "https://esm.sh/yaml-cfn@0.3.1";
+import { yamlParse } from "https://cdn.skypack.dev/yaml-cfn@0.3.1";
 import { z } from "https://deno.land/x/zod@v3.18.0/mod.ts";
 
 export { path, yamlParse, z };
